6138632ba2774e4fda4b799340ecfa3cf2eaf7e2
Ubuntu <ubuntu@ip-10-240-220-201.us-west-2.compute.internal>
2024-07-28 03:16:32 +0000

    Update FiberBagSpec to use ZIO test framework

--- core/shared/src/test/scala/zio/internal/FiberBagSpec.scala
+++ core/shared/src/test/scala/zio/internal/FiberBagSpec.scala
@@ -1,11 +1,11 @@
 package zio.internal
 
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-import zio.{Fiber, FiberId, UIO, Chunk, Exit, Trace, ZIO, Runtime, Unsafe}
-import zio.test.TestAspect._
+import zio._
+import zio.test._
+import zio.test.Assertion._
+import zio.internal.FiberBag
 
-class FiberBagSpec extends AnyFlatSpec with Matchers {
+object FiberBagSpec extends ZIOSpecDefault {
   // Mock Fiber class for testing
   class MockFiber(val id: FiberId) extends Fiber[Nothing, Unit] {
     override def await(implicit trace: Trace): UIO[Exit[Nothing, Unit]] = ZIO.succeed(Exit.unit)
@@ -15,67 +15,58 @@ class FiberBagSpec extends AnyFlatSpec with Matchers {
     override def interruptAsFork(fiberId: FiberId)(implicit trace: Trace): UIO[Unit] = ZIO.unit
   }
 
-  "FiberBag" should "add and remove fibers" in {
-    val bag = new FiberBag()
-    val fiber = new MockFiber(FiberId.None)
+  def spec = suite("FiberBag")(
+    test("add and remove fibers") {
+      for {
+        bag <- ZIO.succeed(new FiberBag())
+        fiber = new MockFiber(FiberId.None)
+        _ <- ZIO.succeed(bag.add(fiber))
+        containsFiber <- ZIO.succeed(bag.iterate().toList.contains(fiber))
+        _ <- ZIO.succeed(bag.remove(fiber))
+        doesNotContainFiber <- ZIO.succeed(!bag.iterate().toList.contains(fiber))
+      } yield assertTrue(containsFiber && doesNotContainFiber)
+    },
 
-    bag.add(fiber)
-    bag.iterate().toList should contain(fiber)
+    test("handle garbage collected fibers") {
+      for {
+        bag <- ZIO.succeed(new FiberBag())
+        fiber = new MockFiber(FiberId.None)
+        _ <- ZIO.succeed(bag.add(fiber))
+        _ <- ZIO.succeed(System.gc())
+        _ <- ZIO.succeed(bag.cleanUp())
+        doesNotContainFiber <- ZIO.succeed(!bag.iterate().toList.contains(fiber))
+      } yield assertTrue(doesNotContainFiber)
+    },
 
-    bag.remove(fiber)
-    bag.iterate().toList should not contain fiber
-  }
+    test("support concurrent addition and removal of fibers") {
+      for {
+        bag <- ZIO.succeed(new FiberBag())
+        fibers = (1 to 1000).map(_ => new MockFiber(FiberId.None))
+        _ <- ZIO.foreachDiscard(fibers)(fiber => ZIO.succeed(bag.add(fiber)))
+        size <- ZIO.succeed(bag.iterate().size)
+        _ <- ZIO.foreachDiscard(fibers)(fiber => ZIO.succeed(bag.remove(fiber)))
+        finalSize <- ZIO.succeed(bag.iterate().size)
+      } yield assertTrue(size == 1000 && finalSize == 0)
+    },
 
-  it should "handle garbage collected fibers" in {
-    val bag = new FiberBag()
-    var fiber = new MockFiber(FiberId.None)
+    test("support concurrent iteration over the set of fibers") {
+      for {
+        bag <- ZIO.succeed(new FiberBag())
+        fibers = (1 to 1000).map(_ => new MockFiber(FiberId.None))
+        _ <- ZIO.foreachDiscard(fibers)(fiber => ZIO.succeed(bag.add(fiber)))
+        iteratedFibers <- ZIO.succeed(bag.iterate().toList)
+      } yield assertTrue(iteratedFibers.size == 1000 && iteratedFibers.toSet == fibers.toSet)
+    },
 
-    bag.add(fiber)
-    fiber = null // simulate garbage collection
-    System.gc()
-
-    bag.cleanUp()
-    bag.iterate().toList should not contain fiber
-  }
-
-  it should "support concurrent addition and removal of fibers" in {
-    val bag = new FiberBag()
-    val fibers = (1 to 1000).map(i => new MockFiber(FiberId.None))
-
-    fibers.foreach(fiber => bag.add(fiber))
-    bag.iterate().size should be(1000)
-
-    fibers.foreach(fiber => bag.remove(fiber))
-    bag.iterate().size should be(0)
-  }
-
-  it should "support concurrent iteration over the set of fibers" in {
-    val bag = new FiberBag()
-    val fibers = (1 to 1000).map(i => new MockFiber(FiberId.None))
-
-    fibers.foreach(bag.add)
-    val iteratedFibers = bag.iterate().toList
-
-    iteratedFibers.size should be(1000)
-    iteratedFibers should contain allElementsOf fibers
-  }
-
-  it should "support concurrent operations using ZIO" in {
-    val runtime = Runtime.default
-    Unsafe.unsafe { implicit unsafe =>
-      runtime.unsafe.run(
-        for {
-          bag <- ZIO.succeed(new FiberBag())
-          fibers <- ZIO.foreach(1 to 1000)(_ => ZIO.succeed(new MockFiber(FiberId.None)))
-          _ <- ZIO.foreachParDiscard(fibers)(fiber => ZIO.succeed(bag.add(fiber)))
-          size <- ZIO.succeed(bag.iterate().size)
-          _ <- ZIO.foreachParDiscard(fibers)(fiber => ZIO.succeed(bag.remove(fiber)))
-          finalSize <- ZIO.succeed(bag.iterate().size)
-        } yield {
-          size should be(1000)
-          finalSize should be(0)
-        }
-      ).getOrThrowFiberFailure()
+    test("support concurrent operations") {
+      for {
+        bag <- ZIO.succeed(new FiberBag())
+        fibers <- ZIO.foreach(1 to 1000)(_ => ZIO.succeed(new MockFiber(FiberId.None)))
+        _ <- ZIO.foreachParDiscard(fibers)(fiber => ZIO.succeed(bag.add(fiber)))
+        size <- ZIO.succeed(bag.iterate().size)
+        _ <- ZIO.foreachParDiscard(fibers)(fiber => ZIO.succeed(bag.remove(fiber)))
+        finalSize <- ZIO.succeed(bag.iterate().size)
+      } yield assertTrue(size == 1000 && finalSize == 0)
     }
-  } @@ timeout(5.seconds)
+  ) @@ timeout(5.seconds)
 }
